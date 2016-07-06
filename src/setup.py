b699044d8d4dbe1c6919eaf0c0a29f5f4cb8748b
michel <michel@place.com>
2016-07-06 10:09:03 -0400

    Changed the version parsing function

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,7 @@
 import shlex
 import subprocess
-import sys
+# import sys
+import re
 
 from setuptools import setup, find_packages
 
@@ -11,55 +12,17 @@ class SetupException(Exception):
     pass
 
 
-def git_unstash():
-    global git_stashed
-
-    if git_stashed:
-
-        cmd_git_unstash = "git stash pop"
-        subprocess.check_call(cmd_git_unstash)
-
-        print "###############################"
-        print "# Restored uncomitted changes"
-        print "###############################"
-
-
-def git_stash():
-    global git_stashed
-
-    cmd_git_stash = "git stash"
-    print ("#####################################################"
-           "##########################################")
-    print ("# Git repo is dirty will stash un-commited changes and "
-           "restore them once the package is created")
-    print ("#####################################################"
-           "##########################################")
-    subprocess.check_call(cmd_git_stash)
-
-    git_stashed = True
-
-
 def set_git_version():
     try:
         cmd_git_version = "git describe --tags --dirty"
         cmd_git_hash = "git rev-parse HEAD"
 
         git_version = subprocess.check_output(shlex.split(cmd_git_version))
-        if git_version.find('dirty') != -1 and '--force' not in sys.argv:
-            git_stash()
-            raise SetupException("Git repository is in a dirty state. "
-                                 "Not all changes have been commited. "
-                                 "Use --force option to force the creation "
-                                 "of the package")
-
-        if '--force' in sys.argv:
-            sys.argv.remove('--force')
-            cmd_git_version = "git describe --tags "
-            git_version = subprocess.check_output(shlex.split(cmd_git_version))
-
+        ver = parse_version(git_version)
         git_hash = subprocess.check_output(shlex.split(cmd_git_hash))
 
         with open('VERSION', 'w') as fp:
+            fp.write(ver + "\n")
             fp.write(git_version)
             fp.write(git_hash)
 
@@ -71,20 +34,26 @@ def set_git_version():
 
 
 def parse_version(git_version):
-    split_version = git_version.split('-')
-    try:
-
-        (major, minor) = split_version[0].split('.')
-
-        if len(split_version) == 3:
-            micro = split_version[1]
-        else:
-            micro = '0'
-
-        return ".".join([major, minor, micro])
-    except ValueError:
+    rem = re.match("^(?P<major>\d+)\.(?P<minor>\d+)"
+                   "(?:$|[-.](?:(?P<micro>\d+)|(?P<dirty>dirty)$)"
+                   "(?:.*?)(?:$|-(?P<dirty1>dirty)$))", git_version)
+    if not rem:
         raise SetupException("The git tag must be set to a <number>.<number>. "
-                             "Currently set to {}".format(split_version[0]))
+                             "Currently set to {}".format(git_version))
+
+    rev = rem.groupdict()
+    try:
+        if rev['micro'] is None:
+            rev['micro'] = 0
+    except:
+        rev['micro'] = 0
+
+    print rev
+    ver = [str(rev['major']), str(rev['minor']), str(rev['micro'])]
+    if rev['dirty'] is not None or rev['dirty1'] is not None:
+        ver.append('dirty')
+
+    return ".".join(ver)
 
 
 def get_version():
@@ -96,25 +65,28 @@ def get_version():
     except:
         raise SetupException("Unable to read the version file. something "
                              "unexpected happened in the prep function")
-    version = parse_version(version)
 
     return version
 
-setup(
-    name="tdev",
-    version=get_version(),
-    packages=find_packages(),
-    requires=[
-        'pip',
-        'virtualenvwraper'
-    ],
-    scripts=['bin/tdev'],
-    entry_points={
-        'console_scripts': ['mkpkg=pkgtools.mkpkg:main',
-                            'mksetup=pkgtools.mksetup:main']
-    },
-    include_package_data=True,
-)
+
+def main():
+
+    setup(
+        name="tdev",
+        version=get_version(),
+        packages=find_packages(),
+        requires=[
+            'pip',
+            'virtualenvwraper'
+        ],
+        scripts=['bin/tdev'],
+        entry_points={
+            'console_scripts': ['mkpkg=pkgtools.mkpkg:main',
+                                'mksetup=pkgtools.mksetup:main']
+        },
+        include_package_data=True,
+    )
 
 
-git_unstash()
+if __name__ == "__main__":
+    main()
---
+++ src/test_setup.py
@@ -0,0 +1,44 @@
+import unittest
+
+from setup import parse_version, SetupException
+
+
+class TestParseVersion(unittest.TestCase):
+
+
+    def test_parseversion(self):
+        version = {}
+        version['1.0.0'] = '1.0.0'
+        version['1.0'] = '1.0.0'
+        version['1.0-3-gd5f2eac-dirty'] = '1.0.3.dirty'
+        version['1.2'] = '1.2.0'
+        version['1.2-dirty'] = '1.2.0.dirty'
+        version['1.2.3'] = '1.2.3'
+        version['1.2.3-dirty'] = '1.2.3.dirty'
+        version['1.2-3-gd5f2eac'] = '1.2.3'
+        version['1.2-3-gd5f2eac-dirty'] = '1.2.3.dirty'
+        for k, v in version.iteritems():
+            print k, v
+            version = parse_version(k)
+            print version
+            self.assertEqual(v, version, "version does not match")
+
+    def test_invalid_values(self):
+        version = []
+        version.append('akfjaksjfasf')
+        version.append('1')
+        version.append('1.a')
+        version.append('a.a')
+        version.append('1.2-sdfkjsfkjskfj')
+
+        for k in version:
+            print k
+            self.assertRaises(SetupException, parse_version, k)
+
+
+# def main():
+#     unittest.main()
+#
+#
+# if __name__ == "__main__":
+#     main()
