09b0b68a56677ed320b2067a9e94f7524743f885
michel <Michel Gauthier>
2018-04-14 21:03:12 +0200

    Update to mkpkg

--- src/pkgtools/mkpkg.py
+++ src/pkgtools/mkpkg.py
@@ -1,43 +1,61 @@
-import os
 import argparse
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
 
-def mkpkg(pkgar):
+def mkpkg(pkgar, base_path="./", version=None):
+    assert isinstance(pkgar, list)
 
-    pkg = os.path.join(*pkgar)
+    tmp_dir = os.getcwd()
+    try:
+        if not os.path.exists(base_path):
+            os.makedirs(base_path)
+        elif not os.path.isdir(base_path):
+            raise ("{} not a directory".format(base_path))
 
-    if not os.path.exists(pkg):
-        os.mkdir(pkg)
-    else:
-        logger.info("path already exists")
+        os.chdir(base_path)
 
-    init_file = os.path.join(pkg, "__init__.py")
+        pkg = os.path.join(*pkgar)
 
-    if not os.path.isfile(init_file):
-        fp = open(init_file, "w")
-        fp.close()
-    else:
-        logger.info("init file already there")
+        if not os.path.exists(pkg):
+            os.mkdir(pkg)
+        else:
+            logger.info("path already exists")
+
+        init_file = os.path.join(pkg, "__init__.py")
+
+        if not os.path.isfile(init_file):
+            with open(init_file, 'w') as fp:
+                if version:
+                    fp.write("__VERSION__ = \"{}\"".format(version))
+
+        else:
+            logger.info("init file already there")
+    finally:
+        os.chdir(tmp_dir)
 
 
 def main():
 
     p = argparse.ArgumentParser()
-    p.add_argument("package_name", help="the name of the package to create in"
-                   " path notation")
+    p.add_argument("package_name", help="package name ie: pkgtools.test")
+    p.add_argument("-base_path",
+                   help="folder in which to create the package",
+                   default="./")
+    p.add_argument("-version", help="version number for the package",
+                   default=None)
 
     args = p.parse_args()
 
     path_list = []
 
-    for i in os.path.split(args.package_name):
-
+    for i in args.package_name.split("."):
         if len(i) != 0:
             path_list.append(i)
-            mkpkg(path_list)
+            mkpkg(path_list, args.base_path, args.version)
+
 
 if __name__ == "__main__":
     main()
