abada643ecefc29671c5832802f48c4e30c191a7
michel <Michel Gauthier>
2018-04-15 14:27:16 +0200

    Add update version tool

--- src/pkgtools/mkpkg.py
+++ src/pkgtools/mkpkg.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import os
+import re
 
 logger = logging.getLogger(__name__)
 
@@ -44,6 +45,40 @@ def make_package(package_name, base_path="./", version=None):
         if len(i) != 0:
             path_list.append(i)
             _mkpkg(path_list, base_path, version)
+
+
+def update_version(package_name, version, base_path="./"):
+
+    pkg_list = package_name.split(".")
+    init_file = os.path.join(base_path, *pkg_list, "__init__.py")
+
+    if not os.path.isfile(init_file):
+        raise Exception("{} is not a valid __init__.py file".format(init_file))
+
+    with open(init_file, 'r') as fp:
+        orig_init = fp.read()
+
+    new_init, count = re.subn("__VERSION__ = \"(.+)\"",
+                              "__VERSION__ = \"{}\"".format(version),
+                              orig_init)
+    if count == 0:
+        new_init = "__VERSION__ = \"{}\"\n".format(version) + new_init
+
+    with open(init_file, 'w') as fp:
+        fp.write(new_init)
+
+
+def main_update():
+    p = argparse.ArgumentParser()
+    p.add_argument("package_name", help="package name ie: pkgtools.test")
+    p.add_argument("version", help="version number for the package",
+                   default=None)
+    p.add_argument("-base_path",
+                   help="folder in which to create the package",
+                   default="./")
+    args = p.parse_args()
+
+    update_version(args.package_name, args.version, args.base_path)
 
 
 def main():
