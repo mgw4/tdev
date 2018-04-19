3722cb10ef50ad8569933529f41eac4cc1f76272
michel <Michel Gauthier>
2018-04-19 10:26:52 +0200

    Adds git and venv to mkskel
     - also ads sensible default path

--- src/pkgtools/mkskel.py
+++ src/pkgtools/mkskel.py
@@ -1,5 +1,8 @@
 import argparse
 import os
+import subprocess
+import shlex
+import logging
 
 from pkgtools.mksetup import (make_setup,
                               make_manifest,
@@ -7,6 +10,31 @@ from pkgtools.mksetup import (make_setup,
                               make_requirements,
                               make_gitignore)
 from pkgtools.mkpkg import make_package
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def make_git(project_path):  # pragma: nocover  check_call will fail
+    # make sure there is a / at the end
+    project_path = os.path.join(project_path, "")
+    cmd = "git -C {project_path} init".format(project_path=project_path)
+    subprocess.check_call(shlex.split(cmd))
+    logger.info("created git repository")
+
+
+def make_venv(project_name, package_list=[]):  # pragma: nocover
+    assert isinstance(package_list, list)
+    default_packages = " -i ipython -i ipdb -i autopep8 -i flake8"
+    packages = default_packages + " -i ".join(package_list)
+    cmd = ("bash -c '. /usr/local/bin/virtualenvwrapper.sh ; "
+           "mkvirtualenv -p python3 {pkglist} {projname}'").format(
+        projname=project_name,
+        pkglist=packages)
+    logger.info("creating virtual env")
+    subprocess.check_output(shlex.split(cmd))
+    logger.info("successfully created virtual env")
 
 
 def make_skel(project_name, project_path, version):
@@ -29,15 +57,27 @@ def make_skel(project_name, project_path, version):
 
 def main():  # prgama: nocover
 
+    default_path = os.getenv("PROJECT_PATH",
+                             os.path.join(os.getenv("HOME"), "github", "devel")
+                             )
+    default_path = os.path.join(default_path, "{project_name}")
     p = argparse.ArgumentParser(
         description="create a default setup for the project")
     p.add_argument("project_name", help="name of the project")
-    p.add_argument("project_path", help="path of the project")
+    p.add_argument("-project_path", help="path of the project",
+                   default=default_path)
     p.add_argument("-version", help="the initial version of the project",
                    default="0.0.0")
+    p.add_argument("-install_packages",
+                   help="packages to install in venv", nargs='*', default=[])
 
     args = p.parse_args()
+    args.project_path = args.project_path.format(
+        project_name=args.project_name)
+    logger.info("creating project in {}".format(args.project_path))
     make_skel(args.project_name, args.project_path, args.version)
+    make_venv(args.project_name, args.install_packages)
+    make_git(args.project_path)
 
 
 if __name__ == "__main__":  # pragma: nocover
